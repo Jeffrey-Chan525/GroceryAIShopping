b529065a7d337bc6c7d24f0e613ddb4a4c01bad4
thewanderer-123 <118376809+thewanderer-123@users.noreply.github.com>
2026-04-04 18:29:13 +1000

    implemented the ItemDao class

--- smartspend-complete/src/main/java/com/smartspend/dao/ItemDao.java
+++ smartspend-complete/src/main/java/com/smartspend/dao/ItemDao.java
@@ -6,7 +6,9 @@ import org.slf4j.LoggerFactory;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ItemDao implements DAO<Item> {
@@ -44,28 +46,100 @@ public class ItemDao implements DAO<Item> {
             statement.execute();
 
         } catch (SQLException e){
-            log.error("An sqliteException has occurred", e);
+            log.error("An sqliteException has occurred when inserting a new Item: ", e);
         }
     }
 
     @Override
     public void update(Item value) {
-        String query = "UPDATE items SET ";
+        String query = "UPDATE items " +
+                "SET name = ?, " +
+                "category = ?, " +
+                "brand = ?, " +
+                "default_unit = ? " +
+                "WHERE item_id = ?";
+        int NAME = 1;
+        int CATEGORY = 2;
+        int BRAND = 3;
+        int DEFAULT_UNIT = 4;
+        int ITEM_ID = 5;
+        try{
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.setString(NAME, value.getName());
+            statement.setString(CATEGORY, value.getCategory());
+            statement.setString(BRAND, value.getBrand());
+            statement.setString(DEFAULT_UNIT, value.getDefaultUnit());
+            statement.setInt(ITEM_ID, value.getId());
+            statement.execute();
+        } catch (SQLException e ){
+            log.error("An error has occurred when updating an Item: ", e);
+        }
+
     }
 
     @Override
     public void delete(Item value) {
-
+        String query = "DELETE FROM items WHERE item_id = ?";
+        int ITEM_ID = 1;
+        try{
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.setInt(ITEM_ID, value.getId());
+        } catch (SQLException e){
+            log.error("An error has occurred when deleting an Item: ", e);
+        }
     }
 
     @Override
     public List<Item> getAll() {
-        return List.of();
+        String query = "SELECT * from items";
+        int ITEM_ID = 1;
+        int NAME = 2;
+        int CATEGORY = 3;
+        int BRAND = 4;
+        int DEFAULT_UNIT = 5;
+        List<Item> items = new ArrayList<>();
+        try {
+            PreparedStatement statement = connection.prepareStatement(query);
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()){
+                int item_id = resultSet.getInt(ITEM_ID);
+                String name = resultSet.getString(NAME);
+                String category = resultSet.getString(CATEGORY);
+                String brand = resultSet.getString(BRAND);
+                String defaultUnit = resultSet.getString(DEFAULT_UNIT);
+                Item currentItem = new Item(item_id, name, category, brand, defaultUnit);
+                items.add(currentItem);
+            }
+        } catch (SQLException e){
+            log.error("An error has occurred while getting the table: ", e);
+        }
+        return items;
     }
 
     @Override
     public Item get(int id) {
-        return null;
+        String query = "SELECT * FROM items WHERE item_id = ?";
+        int ITEM_ID = 1;
+        int NAME = 2;
+        int CATEGORY = 3;
+        int BRAND = 4;
+        int DEFAULT_UNIT = 5;
+        Item item = null;
+        try{
+            PreparedStatement statement = connection.prepareStatement(query);
+            ResultSet resultSet = statement.executeQuery();
+            if (resultSet.next()){
+                int item_id = resultSet.getInt(ITEM_ID);
+                String name = resultSet.getString(NAME);
+                String category = resultSet.getString(CATEGORY);
+                String brand = resultSet.getString(BRAND);
+                String defaultUnit = resultSet.getString(DEFAULT_UNIT);
+                item = new Item(item_id, name, category, brand, defaultUnit);
+            }
+        } catch (SQLException e){
+            log.error("An error has occurred while getting an Item: ", e);
+        }
+        return item;
     }
 
 
