627c25de800ad68415acb96258604550495c5b00
thewanderer-123 <118376809+thewanderer-123@users.noreply.github.com>
2026-04-08 00:32:25 +1000

    added the testDelete test

--- smartspend-complete/src/test/java/com/smartspend/DAL/TestItemDao.java
+++ smartspend-complete/src/test/java/com/smartspend/DAL/TestItemDao.java
@@ -3,9 +3,8 @@ package com.smartspend.DAL;
 
 import com.smartspend.dao.ItemDao;
 import com.smartspend.model.Item;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.Assertions.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -32,6 +31,39 @@ public class TestItemDao {
         return connection;
     }
 
+    private Item createItemFromQuery(ResultSet resultSet){
+        // these values correspond to the column names in the table
+        int ITEM_ID = 1;
+        int NAME = 2;
+        int CATEGORY = 3;
+        int BRAND = 4;
+        int DEFAULT_UNIT = 5;
+
+        try{
+            if (resultSet.next()){
+                int item = resultSet.getInt(ITEM_ID);
+                String name = resultSet.getString(NAME);
+                String category = resultSet.getString(CATEGORY);
+                String brand = resultSet.getString(BRAND);
+                String defaultUnit = resultSet.getString(DEFAULT_UNIT);
+                return new Item(item, name, category, brand, defaultUnit);
+            }
+        } catch (SQLException e){
+            log.error("an error has occurred when creating an Item");
+        }
+        return null;
+    }
+
+    private String ItemToString(Item item){
+        String res = "";
+        res += "Id: " + item.getId() + "\n";
+        res += "Name: " + item.getName() + "\n";
+        res += "Category: " + item.getCategory() + "\n";
+        res += "Brand: " + item.getBrand() + "\n";
+        res += "defaultUnit: " + item.getDefaultUnit() + "\n";
+        return res;
+    }
+
     @BeforeAll
     static void BeforeAll(){
         // creating the table
@@ -50,32 +82,117 @@ public class TestItemDao {
         }
     }
 
+    @BeforeEach
+    void init(){
+        String query = "DELETE FROM items";
+        try {
+            Statement statement = connection.createStatement();
+            statement.execute(query);
+        } catch (SQLException e ){
+            log.error("something has happen while clearing the data from items", e);
+        }
+
+    }
+
     @Test
     void testInsert() {
         // inserting the item
         Item item = new Item(1,"toilet paper", "Household Essentials","Quilton","roll");
         itemDao.insert(item);
-
         //checking if the item is inserted
         String query = "SELECT * FROM items WHERE item_id = 1";
         try{
             PreparedStatement statement = connection.prepareStatement(query);
             ResultSet resultSet = statement.executeQuery();
-            if (resultSet.next()){
-                int item_id = resultSet.getInt(1);
-                String name = resultSet.getString(2);
-                String category = resultSet.getString(3);
-                String brand = resultSet.getString(4);
-                String defaultUnit = resultSet.getString(5);
-                Item inserteditem = new Item(item_id, name, category, brand, defaultUnit);
-                Assertions.assertEquals(item, inserteditem);
-            }
+            Item insertedItem = createItemFromQuery(resultSet);
+            Assertions.assertEquals(item, insertedItem);
         } catch (SQLException e){
-            log.error("Error has occurred");
+            log.error("Error has occurred for insertTest", e);
         }
     }
-
+    // this test is disabled until the equals method is implemented in Item
+    @Disabled
     @Test
     void testUpdate(){
+            String TestDataQuery = """
+                    INSERT INTO items (item_id, name, category, default_unit) VALUES (?, ?, ?, ?)
+                    """;
+            try {
+                // inserting the test data into the table
+                PreparedStatement statement = connection.prepareStatement(TestDataQuery);
+                statement.setInt(1, 2);
+                statement.setString(2, "toilet Paper");
+                statement.setString(3, "household products");
+                statement.setString(4, "rolls");
+                statement.execute();
+
+                // retrieving the inserted Test data
+                String query = "SELECT * from items WHERE item_id = 2";
+                Statement retrievedDataStatement = connection.createStatement();
+                ResultSet resultSet = retrievedDataStatement.executeQuery(query);
+                // updating the data with ItemDAO
+                int ITEM_ID = 1;
+                int NAME = 2;
+                int CATEGORY = 3;
+                int DEFAULT_UNIT = 5;
+                Item Expected = null;
+                if (resultSet.next()){
+                    int item_id = resultSet.getInt(ITEM_ID);
+                    String name = resultSet.getString(NAME);
+                    String category = resultSet.getString(CATEGORY);
+                    String defaultUnit = resultSet.getString(DEFAULT_UNIT);
+                    Expected = new Item(item_id, name, category, "quilton", defaultUnit);
+                    itemDao.update(Expected);
+
+                }
+
+                ResultSet updatedResultSet = retrievedDataStatement.executeQuery(query);
+                Item ActualUpdateItem = createItemFromQuery(updatedResultSet);
+                Assertions.assertSame(Expected, ActualUpdateItem);
+
+            } catch (SQLException e){
+                log.error("error occurred from updateTest", e);
+            }
+
+    }
+    @Test
+    void testDelete(){
+        String insertTestData = "INSERT INTO items (item_id, name, category, default_unit) values (?,?,?,?)";
+        //inserts the data to be deleted
+        int ID_VALUE = 1;
+        String NAME_VALUE = "toilet paper";
+        String CATEGORY_VALUE = "household items";
+        String BRAND_VALUE = "quilton";
+        String DEFAULT_UNIT_VALUE = "rolls";
+        try{
+            PreparedStatement preparedStatement = connection.prepareStatement(insertTestData);
+            int ITEM_ID = 1;
+            int NAME = 2;
+            int CATEGORY = 3;
+            int BRAND = 4;
+            int DEFAULT_UNIT = 5;
+
+            preparedStatement.setInt(ITEM_ID, ID_VALUE);
+            preparedStatement.setString(NAME, NAME_VALUE);
+            preparedStatement.setString(CATEGORY, CATEGORY_VALUE);
+            preparedStatement.setString(BRAND, BRAND_VALUE);
+            preparedStatement.setString(DEFAULT_UNIT, DEFAULT_UNIT_VALUE);
+        } catch (SQLException e){
+            log.error("There was an error with inserting the test data",e );
+        }
+        // deletes the data with ItemDAO
+        Item item = new Item(ID_VALUE, NAME_VALUE, CATEGORY_VALUE, BRAND_VALUE, DEFAULT_UNIT_VALUE);
+        itemDao.delete(item);
+
+        String selectTableQuery = "SELECT * FROM items";
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(selectTableQuery);
+            if (!resultSet.next()){
+                Assertions.fail();
+            }
+        } catch (SQLException e){
+            log.error("an error has occurred while selecting data from the table");
+        }
     }
 }
