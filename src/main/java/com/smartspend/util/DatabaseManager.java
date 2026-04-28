e1f9159f414e5f82436f440374cb3c920ec0c53e
thewanderer-123 <118376809+thewanderer-123@users.noreply.github.com>
2026-04-28 22:02:34 +1000

    changed the url of connecting to the database to use an environment variable + docs for setting up the .env file

--- src/main/java/com/smartspend/util/DatabaseManager.java
+++ src/main/java/com/smartspend/util/DatabaseManager.java
@@ -5,6 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseManager {
+// see how to setup the .env file here: docs/setup/environment-variables-SETUP.md
     private static final String LOCAL_DB_URL = System.getenv("DATABASE_URL");
     private static final String DB_URL = "jdbc:sqlite:" + LOCAL_DB_URL;
 
