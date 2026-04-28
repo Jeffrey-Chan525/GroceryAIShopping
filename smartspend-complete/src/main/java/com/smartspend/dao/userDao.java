62aca2925b8647be35c39cf4c7dc50fa60e3d9d2
thewanderer-123 <118376809+thewanderer-123@users.noreply.github.com>
2026-04-28 17:54:20 +1000

    creating the userDao class

---
+++ smartspend-complete/src/main/java/com/smartspend/dao/userDao.java
@@ -0,0 +1,32 @@
+package com.smartspend.dao;
+
+import com.smartspend.model.User;
+
+import java.util.List;
+
+public class userDao implements DAO<User> {
+    @Override
+    public void insert(User value) {
+
+    }
+
+    @Override
+    public void update(User value) {
+
+    }
+
+    @Override
+    public void delete(User value) {
+
+    }
+
+    @Override
+    public List<User> getAll() {
+        return List.of();
+    }
+
+    @Override
+    public User get(int id) {
+        return null;
+    }
+}
