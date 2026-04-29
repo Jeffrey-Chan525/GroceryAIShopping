176d1ebfbe13354ce89b575b518651bf25f1f31d
thewanderer-123 <118376809+thewanderer-123@users.noreply.github.com>
2026-04-29 11:27:51 +1000

    added UserDao + finished Tests

--- src/main/java/com/smartspend/model/User.java
+++ src/main/java/com/smartspend/model/User.java
@@ -1,5 +1,7 @@
 package com.smartspend.model;
 
+import java.util.Objects;
+
 public class User {
     private int userId;
     private String username;
@@ -14,4 +16,20 @@ public class User {
     public int getUserId() { return userId; }
     public String getUsername() { return username; }
     public String getEmail() { return email; }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        boolean isUserIDEquals = user.userId == this.userId;
+        boolean isUsernameEquals = user.username.equals(this.username);
+        boolean isEmailEquals = user.email.equals(this.email);
+        return isEmailEquals && isUserIDEquals && isUsernameEquals;
+
+    }
+    @Override
+    public int hashCode() {
+        return  Objects.hash(userId);
+    }
 }
