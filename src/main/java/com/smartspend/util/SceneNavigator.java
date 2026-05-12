bdf6f689e695ef99f60454e86c41047baff63191
Eric Rizki <n12233986@qut.edu.au>
2026-05-12 12:15:53 +1000

    Fix window shrinking bug

--- src/main/java/com/smartspend/util/SceneNavigator.java
+++ src/main/java/com/smartspend/util/SceneNavigator.java
@@ -14,8 +14,10 @@ public class SceneNavigator {
 
     public static void switchScene(Node node, String fxmlPath, String title) throws IOException {
         Stage stage = (Stage) node.getScene().getWindow();
+        double width = stage.getWidth();
+        double height = stage.getHeight();
         FXMLLoader loader = new FXMLLoader(SceneNavigator.class.getResource(fxmlPath));
-        Scene scene = new Scene(loader.load(), 1100, 720);
+        Scene scene = new Scene(loader.load(), width, height);
         scene.getStylesheets().add(SceneNavigator.class.getResource("/css/style.css").toExternalForm());
         stage.setTitle(title);
         stage.setScene(scene);
